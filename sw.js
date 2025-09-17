ed88157213c0cfda96ef4e52869511a8735c9213
ivanes921 <ivanes921@gmail.com>
2025-09-17 16:22:33 +0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,30 @@
 const CACHE = "stopwatch-v1";
-const ASSETS = ["./", "./index.html", "./manifest.webmanifest"];
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-180.png",
+  "./icons/icon-512.png",
+  "./fonts/SF-Pro-Display-Thin.woff"
+];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
-self.addEventListener("activate", e=>{
-  e.waitUntil(self.clients.claim());
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
-self.addEventListener("fetch", e=>{
+
+self.addEventListener("fetch", e => {
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
